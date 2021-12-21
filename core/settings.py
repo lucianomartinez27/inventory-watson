24406b89ebcb0e1ea00e88256f4f47c4915ada81
Luciano Martinez <lucianomartinez27@gmail.com>
2021-12-21 09:22:22 -0300

    changed key and debug as env var

--- core/settings.py
+++ core/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qyu(9l9v%^+r(vt#ecf+36#lis516#3bo5@bo-rd*d%a=!%8#!'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('IS_HEROKU', False)
 
 ALLOWED_HOSTS = ['warm-woodland-71021.herokuapp.com', '127.0.0.1']
 
